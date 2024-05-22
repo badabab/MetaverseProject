03f5b3cb5b4af93e7f95e964b2da2067adcf4bd5
badabab <ky040723@gmail.com>
2024-05-22 12:27:39 +0900

    [김예은] 플레이어 하는 중

--- Assets/02.Scripts/Player/PlayerMoveAbility.cs
+++ Assets/02.Scripts/Player/PlayerMoveAbility.cs
@@ -5,7 +5,7 @@ public class PlayerMoveAbility : PlayerAbility
     public float MoveSpeed = 5f;
     private CharacterController _characterController;
 
-    private float _gravity = -20f;
+    private float _gravity = -9.8f;
     private float _yVelocity = 0f;
 
     private void Start()
@@ -23,7 +23,8 @@ public class PlayerMoveAbility : PlayerAbility
         float v = Input.GetAxisRaw("Vertical");
 
         Vector3 dir = new Vector3(h, 0, v);
-        dir = dir.normalized;
+        dir.Normalize();
+        dir = Camera.main.transform.TransformDirection(dir);
 
         _yVelocity += _gravity * Time.deltaTime;
         dir.y = _yVelocity;

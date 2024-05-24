d9d1bd563e415e5c5d6a64af5f0e3e650ea5b247
공지수 <giroo27@gmail.com>
2024-05-24 16:54:28 +0900

    [공지수] 상점에 가서 플레이어 교체하기

---
+++ Assets/02.Scripts/Scene/ShoppingAvatar.cs
@@ -0,0 +1,72 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShoppingAvatar : MonoBehaviourPunCallbacks
+{
+    public TextMeshProUGUI PopUpText;
+    public SphereCollider SphereCollider;
+    public GameObject ChangeAvatarButton;
+    public List<GameObject> Avatars; // 아바타 프리팹 리스트
+    private GameObject currentAvatar;
+    private GameObject player;
+
+    private void Start()
+    {
+        PopUpText.gameObject.SetActive(false);
+        ChangeAvatarButton.SetActive(false);
+        ChangeAvatarButton.GetComponent<Button>().onClick.AddListener(OnClickChanging);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && other.GetComponentInParent<PhotonView>().IsMine)
+        {
+            player = other.gameObject;
+            PopUpText.gameObject.SetActive(true);
+            ChangeAvatarButton.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && other.GetComponentInParent<PhotonView>().IsMine)
+        {
+            PopUpText.gameObject.SetActive(false);
+            ChangeAvatarButton.SetActive(false);
+            player = null;
+        }
+    }
+
+    public void OnClickChanging()
+    {
+        if (player != null)
+        {
+            if (currentAvatar != null)
+            {
+                PhotonNetwork.Destroy(currentAvatar);
+            }
+
+            int randomIndex = Random.Range(0, Avatars.Count);
+            Vector3 spawnPosition = player.transform.position;
+            Quaternion spawnRotation = player.transform.rotation;
+
+            currentAvatar = PhotonNetwork.Instantiate(Avatars[randomIndex].name, spawnPosition, spawnRotation, 0);
+            currentAvatar.GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.LocalPlayer);
+
+            // 기존 플레이어 오브젝트를 제거
+            PhotonNetwork.Destroy(player);
+
+            // UI 숨김
+            PopUpText.gameObject.SetActive(false);
+            ChangeAvatarButton.SetActive(false);
+        }
+    }
+    public void OnClickXButton()
+    {
+        ChangeAvatarButton.SetActive(false);
+    }
+}

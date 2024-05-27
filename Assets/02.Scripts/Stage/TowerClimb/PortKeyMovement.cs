865181d4ec20414a0f173938c81afb4557b676e7
dbstjr7412369@gmail.com <dbstjr7412369@gmail.com>
2024-05-27 10:55:44 +0900

    [이윤석] 펌킨 제작 중

---
+++ Assets/02.Scripts/Stage/TowerClimb/PortKeyMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PortKeyMovement : MonoBehaviour
+{
+    [SerializeField]
+    public List<GameObject> PortKey;
+    public InputField PortKeyPassword_text;
+    public float PortKeySpeed;
+    public int PortKeyPassword = 4885;
+    private int incorrectPasswordAttempts = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PortKeyMovement();
+    }
+
+    public void PortKeyMovement()
+    {
+
+    }
+    public void OnPortKeyPassword()
+    {
+        int Password;
+        bool isNumber = int.TryParse(PortKeyPassword_text.text, out Password);
+        {
+            if (isNumber && Password != PortKeyPassword)
+            {
+                incorrectPasswordAttempts++;
+                Debug.Log("패스워드가 틀렸습니다.");
+                if (incorrectPasswordAttempts == 3)
+                {
+                    Debug.Log("관계자가 아니시군요.");
+                }
+                else if (incorrectPasswordAttempts == 5)
+                {
+                    Debug.Log("잔꾀부리지 마세요.");
+                }
+                return;
+            }
+            incorrectPasswordAttempts = 0;
+        }
+    }
+}

7b50719ce79c244e3ebed843aa1f25ad4a6bf0be
공지수 <giroo27@gmail.com>
2024-05-27 11:30:19 +0900

    [공지수] 누겟포유니티 다운로드

--- Assets/02.Scripts/Server/MongoDB.cs
+++ Assets/02.Scripts/Server/MongoDB.cs
@@ -1,13 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
+/*using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 
 public class MongoDB : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        string connnectionString = "mongodb+srv://MetaversePro:MetaversePro@cluster0.ed1au27.mongodb.net/";
+        MongoClient mongoClient = new MongoClient(connnectionString);
+        List<BsonDocument> dbList = mongoClient.ListDatabases().ToList();
+        IMongoDatabase sampleDB = mongoClient.GetDatabase("Logins");
     }
 
     // Update is called once per frame
@@ -16,3 +21,4 @@ public class MongoDB : MonoBehaviour
         
     }
 }
+*/

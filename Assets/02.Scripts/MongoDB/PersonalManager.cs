169db1f4f81b3ee537fb93461abf2f0b5dd4f7ca
공지수 <giroo27@gmail.com>
2024-05-27 12:45:44 +0900

    [공지수] 로그인하면 몽고디비에 저장하는 스크립트까지

---
+++ Assets/02.Scripts/MongoDB/PersonalManager.cs
@@ -0,0 +1,48 @@
+using MongoDB.Driver;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonalManager : MonoBehaviour
+{
+    // 게시글 리스트
+    private List<Personal> _personal = new List<Personal>();
+    public List<Personal> Personals => _personal;
+    // 콜렉션
+    private IMongoCollection<Personal> _personalCollection;
+    public static PersonalManager Instance { get; private set; }
+    private void Awake()
+    {
+        Instance = this;
+        Init();
+    }
+
+    public void Init()
+    {
+        string connnectionString = "mongodb+srv://MetaversePro:MetaversePro@cluster0.ed1au27.mongodb.net/";
+        MongoClient mongoClient = new MongoClient(connnectionString);        
+        IMongoDatabase db = mongoClient.GetDatabase("Logins");
+        // 3. 특정 콜렉션 연결
+        _personalCollection = db.GetCollection<Personal>("Log");
+    }
+
+    public void JoinList(string name, string password)
+    {
+        Personal personal = new Personal()
+        {
+
+            Name = name,
+            Password = password,
+        };
+        _personalCollection.InsertOne(personal);
+    }
+    public Personal Login(string name, string password)
+    {
+        var filter = Builders<Personal>.Filter.Eq("Name", name) & Builders<Personal>.Filter.Eq("Password", password);
+        return _personalCollection.Find(filter).FirstOrDefault();
+    }
+    public bool CheckUser(string name, string password)
+    {
+        var filter = Builders<Personal>.Filter.Eq("Name", name) & Builders<Personal>.Filter.Eq("Password", password);
+        return _personalCollection.Find(filter).Any();
+    }
+}

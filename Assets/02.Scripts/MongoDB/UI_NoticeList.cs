a1723230a0cd7b0a7122e982bd57773dcdcaaebd
dbstjr7412369@gmail.com <dbstjr7412369@gmail.com>
2024-05-28 09:36:35 +0900

    [이윤석] 공지사항 작성 중

--- Assets/02.Scripts/MongoDB/UI_NoticeList.cs
+++ Assets/02.Scripts/MongoDB/UI_NoticeList.cs
@@ -14,14 +14,46 @@ public class UI_NoticeList : MonoBehaviour
 
     public static UI_Notice Instance { get; private set; }
 
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     public void Show()
     {
         gameObject.SetActive(true);
     }
 
-    public void OnClickNoticeButton()
-    {
 
+
+    public void OnClickNoticeButton(int index)
+    {
+        // NoticeText 리스트의 모든 텍스트를 비활성화
+        for (int i = 0; i < NoticeText.Count; i++)
+        {
+            NoticeText[i].gameObject.SetActive(false);
+        }
+
+        // 클릭된 버튼의 인덱스에 해당하는 텍스트를 활성화
+        if (index >= 0 && index < NoticeText.Count)
+        {
+            NoticeText[index].gameObject.SetActive(true);
+        }
+
+        // 버튼 이미지 컬러 변경
+        for (int i = 0; i < NoticeButton.Count; i++)
+        {
+            ColorBlock colorBlock = NoticeButton[i].colors;
+            if (i == index)
+            {
+                colorBlock.normalColor = new Color(1, 1, 1); // RGB 255,255,255
+            }
+            else
+            {
+                colorBlock.normalColor = new Color(0.78f, 0.78f, 0.78f); // RGB 200,200,200
+            }
+            NoticeButton[i].colors = colorBlock;
+        }
     }
 
     public void OnClickOutButton()
@@ -41,4 +73,15 @@ public class UI_NoticeList : MonoBehaviour
     {
         gameObject.SetActive(false);
     }
+
+    private void Start()
+    {
+        // 각 버튼에 클릭 이벤트 리스너 추가
+        for (int i = 0; i < NoticeButton.Count; i++)
+        {
+            int index = i; // 로컬 변수로 인덱스 캡처
+            NoticeButton[i].onClick.AddListener(() => OnClickNoticeButton(index));
+        }
+    }
+
 }

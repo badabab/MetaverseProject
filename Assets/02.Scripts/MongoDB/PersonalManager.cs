b9d8a3e60e4783b2eaa3c4b5188e1c0eab787e18
공지수 <giroo27@gmail.com>
2024-05-28 12:32:44 +0900

    [공지수] 스크립트 충돌나서 날아간거 수정

--- Assets/02.Scripts/MongoDB/PersonalManager.cs
+++ Assets/02.Scripts/MongoDB/PersonalManager.cs
@@ -23,7 +23,6 @@ public class PersonalManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
     public void Init()
     {
         string connnectionString = "mongodb+srv://MetaversePro:MetaversePro@cluster0.ed1au27.mongodb.net/";
@@ -35,7 +34,7 @@ public class PersonalManager : MonoBehaviour
 
     public void JoinList(string name, string password, int characterIndex)
     {
-<<<<<<< HEAD
+
         Personal personal = new Personal()
         {
             Name = name,
@@ -43,24 +42,6 @@ public class PersonalManager : MonoBehaviour
             CharacterIndex = characterIndex
         };
         _personalCollection.InsertOne(personal);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 8244951 ([이윤석] 공지사항 게시판으로 변경 중)
-=======
-            Personal personal = new Personal()
-            {
-                Name = name,
-                Password = password,
-            };
-            _personalCollection.InsertOne(personal);
-        PlayerCanvasAbility.Instance.SetNickname(name);
->>>>>>> 7ecaa3ce7e7a3cbe49fee68fa783cf66758e7592
-<<<<<<< HEAD
->>>>>>> 8244951 ([이윤석] 공지사항 게시판으로 변경 중)
-=======
->>>>>>> 8244951 ([이윤석] 공지사항 게시판으로 변경 중)
     }
     public Personal Login(string name, string password)
     {
@@ -72,12 +53,22 @@ public class PersonalManager : MonoBehaviour
         var filter = Builders<Personal>.Filter.Eq("Name", name) & Builders<Personal>.Filter.Eq("Password", password);
         return _personalCollection.Find(filter).Any();
     }
-    public void UpdateCharacterIndex(string name, int characterIndex)
-<<<<<<< HEAD
+    public void UpdateCharacterIndex(int characterIndex)
     {
-        var filter = Builders<Personal>.Filter.Eq("Name", name);
-        var update = Builders<Personal>.Update.Set("CharacterIndex", characterIndex);
-        _personalCollection.UpdateOne(filter, update);
+        string name = PlayerPrefs.GetString("LoggedInId");
+
+        if (string.IsNullOrEmpty(name))
+        {
+            Debug.LogError("사용자 이름을 찾을 수 없습니다.");
+            return;
+        }
+
+        var filter = Builders<Personal>.Filter.Eq(p => p.Name, name);
+        var update = Builders<Personal>.Update.Set(p => p.CharacterIndex, characterIndex);
+
+        var result = _personalCollection.UpdateOne(filter, update);
+        Debug.Log("Matched Count: " + result.MatchedCount);
+        Debug.Log("Modified Count: " + result.ModifiedCount);
     }
     public int CheckCharacterIndex()
     {
@@ -99,12 +90,5 @@ public class PersonalManager : MonoBehaviour
         }
         else
         { return -1; }
-
-=======
-    {
-        var filter = Builders<Personal>.Filter.Eq("Name", name);
-        var update = Builders<Personal>.Update.Set("CharacterIndex", characterIndex);
-        _personalCollection.UpdateOne(filter, update);
->>>>>>> 8244951 ([이윤석] 공지사항 게시판으로 변경 중)
     }
 }

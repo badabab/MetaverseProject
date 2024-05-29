b03f80b606065a8da6a39609852675d716401ced
공지수 <giroo27@gmail.com>
2024-05-29 17:58:10 +0900

    [공지수] 닉네임 나오는거 수정

--- Assets/02.Scripts/Scene/VillageScene.cs
+++ Assets/02.Scripts/Scene/VillageScene.cs
@@ -47,21 +47,25 @@ public class VillageScene : MonoBehaviourPunCallbacks
                 PhotonNetwork.Instantiate($"Player {PlayerSelection.Instance.SelectedCharacterIndex}", spawnPoint, Quaternion.identity);
                 string nickname = PlayerPrefs.GetString("LoggedInId");
                 Debug.Log($"{nickname}");
-               // PlayerCanvasAbility.Instance.SetNickname(nickname);
+                PlayerCanvasAbility.Instance.SetNickname(nickname);
+                PlayerCanvasAbility.Instance.ShowMyNickname();
             }
             if (UI_Lobby.SelectedType == PlayerType.Male)
             {
                 PhotonNetwork.Instantiate($"Player {PlayerSelection.Instance.SelectedCharacterIndex}", spawnPoint, Quaternion.identity);
                 string nickname = PlayerPrefs.GetString("LoggedInId");
                 Debug.Log($"{nickname}");
-              //  PlayerCanvasAbility.Instance.SetNickname(nickname);
+                PlayerCanvasAbility.Instance.SetNickname(nickname);
+                PlayerCanvasAbility.Instance.ShowMyNickname();
             }
         }
         else
         {
             PhotonNetwork.Instantiate($"Player {characterIndex}", spawnPoint, Quaternion.identity);
             string nickname = PlayerPrefs.GetString("LoggedInId");
-          //  PlayerCanvasAbility.Instance.NicknameTextUI.text = nickname;
+            PlayerCanvasAbility.Instance.NicknameTextUI.text = nickname;
+            PlayerCanvasAbility.Instance.SetNickname(nickname);
+            PlayerCanvasAbility.Instance.ShowMyNickname();
             Debug.Log($"{nickname}");
         }
     }

035af7595dfc29bc59df079e7667d0af8e6c8260
공지수 <giroo27@gmail.com>
2024-05-29 11:30:04 +0900

    [공지수] 몽고디비 글쓰기 스크립트 수정 중

--- Assets/02.Scripts/MongoDB/BulletinBoard/UI_ArticleWrite.cs
+++ Assets/02.Scripts/MongoDB/BulletinBoard/UI_ArticleWrite.cs
@@ -1,17 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UI_ArticleWrite : MonoBehaviour
 {
+    public Toggle NoticeToggle;
     public UI_ArticeList ArticleListUI;
     public InputField ContentInputFieldUI;
 
+    public static UI_ArticleWrite Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        this.gameObject.SetActive(false);
+    }
     public void OnClickYesButton()
     {
-        ArticleListUI.gameObject.SetActive(true);
-        gameObject.SetActive(false);
+        ArticleType articleType = NoticeToggle.isOn ? ArticleType.Notice : ArticleType.Normal;
+        string name = PlayerPrefs.GetString("LoggedInId");
+        if (string.IsNullOrEmpty(name))
+        {
+            Debug.LogError("사용자 이름을 찾을 수 없습니다.");
+            return;
+        }
+        string content = ContentInputFieldUI.text;
+        if (string.IsNullOrEmpty(content))
+        {
+            return;
+        }
+        ArticleManager.Instance.Write(articleType, name, content);
+        ArticleListUI.gameObject.SetActive(false);
+        gameObject.SetActive(true);
     }
     public void OnClickNoButton()
     {

58add93e70377dec12dca176436560ef96b5c074
dbstjr7412369@gmail.com <dbstjr7412369@gmail.com>
2024-05-30 18:10:41 +0900

    [이윤석] 마을 UI 코인 수정 중

--- Assets/02.Scripts/MongoDB/Village/UI_PlayerCoinCount.cs
+++ Assets/02.Scripts/MongoDB/Village/UI_PlayerCoinCount.cs
@@ -7,19 +7,4 @@ using UnityEngine.UI;
 public class UI_PlayerCoinCount : MonoBehaviour
 {
     public TMP_Text PlayerCoinCount;
-    private GameManager gameManager;
-
-    private void Start()
-    {
-        gameManager = GameManager.Instance;
-
-        gameManager.OnCoinsAdded += UpdateCoinCount;
-    }
-
-    // GameManager의 AddCoinsToWinner 이벤트에 연결된 UI 업데이트 함수
-    private void UpdateCoinCount(string playerId, int newCoinCount)
-    {
-        // PlayerCoinCount에 새로운 코인 수를 표시
-        PlayerCoinCount.text = newCoinCount.ToString();
-    }
 }

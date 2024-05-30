9e09661d7db41a2675d4141f5d2c4f051a4527cc
dbstjr7412369@gmail.com <dbstjr7412369@gmail.com>
2024-05-30 15:56:58 +0900

    [이윤석] 마을 UI 코드 작성 중

---
+++ Assets/02.Scripts/MongoDB/Village/UI_PlayerCoinCount.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_PlayerCoinCount : MonoBehaviour
+{
+    public Text PlayerCoinCount;
+
+}

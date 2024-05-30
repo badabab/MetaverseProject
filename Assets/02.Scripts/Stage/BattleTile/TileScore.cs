d7c7666a52713b6528ba874755b1daf07994837a
badabab <ky040723@gmail.com>
2024-05-30 16:31:43 +0900

    [김예은] 타일 타이머

--- Assets/02.Scripts/Stage/BattleTile/TileScore.cs
+++ Assets/02.Scripts/Stage/BattleTile/TileScore.cs
@@ -3,37 +3,34 @@ using UnityEngine;
 
 public class TileScore : MonoBehaviour
 {
+    public static TileScore Instance { get; private set; }
+
     public Material Tile_Blue;
     public Material Tile_Green;
     public Material Tile_Pink;
     public Material Tile_Violet;
 
-    public TextMeshProUGUI P1_Score;
-    public TextMeshProUGUI P2_Score;
-    public TextMeshProUGUI P3_Score;
-    public TextMeshProUGUI P4_Score;
+    public int Player1score;
+    public int Player2score;
+    public int Player3score;
+    public int Player4score;
 
-    private int _player1score;
-    private int _player2score;
-    private int _player3score;
-    private int _player4score;
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     void Update()
     {
-        CountTileObjectsUsingMaterials();
-
-        P1_Score.text = $"{_player1score}";
-        P2_Score.text = $"{_player2score}";
-        P3_Score.text = $"{_player3score}";
-        P4_Score.text = $"{_player4score}";
+        CountTileObjectsUsingMaterials();       
     }
 
     void CountTileObjectsUsingMaterials()
     {
-        _player1score = 0;
-        _player2score = 0;
-        _player3score = 0;
-        _player4score = 0;
+        Player1score = 0;
+        Player2score = 0;
+        Player3score = 0;
+        Player4score = 0;
 
         GameObject[] allObjects = FindObjectsOfType<GameObject>();
 
@@ -48,22 +45,22 @@ public class TileScore : MonoBehaviour
                     {
                         if (mat == Tile_Blue)
                         {
-                            _player1score++;
+                            Player1score++;
                             break;
                         }
                         else if (mat == Tile_Green)
                         {
-                            _player2score++;
+                            Player2score++;
                             break;
                         }
                         else if (mat == Tile_Pink)
                         {
-                            _player3score++;
+                            Player3score++;
                             break;
                         }
                         else if (mat == Tile_Violet)
                         {
-                            _player4score++;
+                            Player4score++;
                             break;
                         }
                     }
---
+++ Assets/02.Scripts/Stage/BattleTile/UI_BattleTile.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+
+public class UI_BattleTile : MonoBehaviour
+{
+    public TextMeshProUGUI P1_Score;
+    public TextMeshProUGUI P2_Score;
+    public TextMeshProUGUI P3_Score;
+    public TextMeshProUGUI P4_Score;
+
+    public TextMeshProUGUI Timer;
+
+    private void Update()
+    {
+        P1_Score.text = $"{TileScore.Instance.Player1score}";
+        P2_Score.text = $"{TileScore.Instance.Player2score}";
+        P3_Score.text = $"{TileScore.Instance.Player3score}";
+        P4_Score.text = $"{TileScore.Instance.Player4score}";
+
+        Timer.text = $"{(int)BattleTileManager.Instance.TimeRemaining}";
+    }
+}

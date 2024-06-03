2a11e7ea92e364cd3d9eb4e39c310b82f014629a
Ohsori001106 <surnameMin@Gmail.com>
2024-06-03 17:59:20 +0900

    [이성민] 플레이어 무브 수정 리지드 바디 실험중

--- Assets/02.Scripts/Player/PlayerMoveAbility.cs
+++ Assets/02.Scripts/Player/PlayerMoveAbility.cs
@@ -4,135 +4,111 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMoveAbility : PlayerAbility
 {
-    private float _currentSpeed;
-    public float MoveSpeed = 5f;
+    public float Movespeed = 5f;
     public float RunSpeed = 15f;
+    public float JumpPower = 3f;
 
-    public bool _IsRunning;
+    public bool isGrounded;		// 땅에 서있는지 체크하기 위한 bool값
+    public LayerMask ground;	// 레이어마스크 설정
+    public float groundDistance = 0.2f;		// Ray를 쏴서 검사하는 거리
 
-    private CharacterController _characterController;
+    private bool _isRunning;
+
+    public Transform LayerPoint;
     private Animator _animator;
+    Rigidbody rb;
+    public Transform CameraRoot;
+    Vector3 dir = Vector3.zero;
 
-    private float _gravity = -9.8f;
-    private float _yVelocity = 0f;
-
-    public float JumpPower = 2.5f;
-    private bool _isJumping = false;
-
-    private bool isFallGuysScene = false; // FallGuysScene 여부 확인
-
-    private void Start()
+    private bool _isFallGuysScene = false; // 폴가이즈 씬인지 확인
+    void Start()
     {
-        _characterController = GetComponent<CharacterController>();
-        _animator = GetComponentInChildren<Animator>();
-
-        // 현재 씬이 FallGuysScene인지 확인
-        isFallGuysScene = SceneManager.GetActiveScene().name == "FallGuysScene";
+        rb = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+        if (_owner.PhotonView.IsMine)
+        {
+            GameObject.FindWithTag("MainCamera").GetComponent<TPSCamera>().target = CameraRoot;
+        }
     }
 
-    private void Update()
+    // 키 입력과 이동방향 계산
+    void Update()
     {
         if (!_owner.PhotonView.IsMine)
         {
             return;
         }
-        HandleMovement();
-        OnAnimatorMove();
+        GroundCheck();
+        InputAndDir();
+        Jump();
+
     }
 
-    private void HandleMovement()
+    // 계산된 방향으로 물리적인 이동 구현
+    private void FixedUpdate()
     {
-/*        if (isFallGuysScene && FallGuysManager.Instance._currentGameState == GameState.Loading)
+       
+
+        
+        
+    }
+
+    // 키 입력과 그에 따른 이동방향을 계산하는 함수
+    void InputAndDir()
+    {
+        dir.x = Input.GetAxis("Horizontal");   // x축 방향 키 입력
+        dir.z = Input.GetAxis("Vertical");     // z축 방향 키 입력
+        Vector3 direction = new Vector3(dir.x, 0f, dir.z);
+        float movementMagnitude = direction.magnitude;
+
+        _animator.SetFloat("Move", Mathf.Clamp01(movementMagnitude));
+
+        rb.velocity =  new Vector3(direction.x,rb.velocity.y, direction.z);
+        if (dir != Vector3.zero)   // 키입력이 존재하는 경우
         {
-            return; // 로딩 중일 때 입력을 무시합니다.
-        }*/
-
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-
-        Vector3 dir = new Vector3(h, 0, v);
-        Vector3 unNormalizedDir = dir;
-        dir.Normalize();
-        dir = Camera.main.transform.TransformDirection(dir);
-
-        if (_characterController.isGrounded)
+            transform.forward = dir;	// 키 입력 시, 입력된 방향으로 캐릭터의 방향을 바꿈
+            _animator.SetBool("Walk", true);
+        }
+        else if (dir == Vector3.zero)
         {
-            _isJumping = false;
-            _yVelocity = 0;
+            _animator.SetBool("Walk", false);
         }
 
-        if (!_characterController.isGrounded)
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            _isJumping = true;
-            _animator.SetBool("RunJump", false);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
-        {
-            Jump();
-            if (_IsRunning)
-            {
-                _animator.SetBool("RunJump", true);
-            }
-            else
-            {
-                _animator.SetTrigger("Jump");
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            _animator.SetTrigger("Punching");
-        }
-
-        if (isFallGuysScene)
-        {
-            _currentSpeed = RunSpeed;
-            if (_animator.GetFloat("Move") >= 1)
-            {
-                _IsRunning = true;
-                _animator.SetBool("Run", true);
-            }
-            else
-            {
-                _IsRunning = false;
-                _animator.SetBool("Run", false);
-            }
+            rb.MovePosition(rb.position + dir * RunSpeed * Time.deltaTime);
+            _isRunning = true;
+            _animator.SetBool("Run", true);
         }
         else
         {
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                _IsRunning = true;
-                _currentSpeed = RunSpeed;
-                _animator.SetBool("Run", true);
-            }
-            else
-            {
-                _IsRunning = false;
-                _currentSpeed = MoveSpeed;
-                _animator.SetBool("Run", false);
-            }
+            rb.MovePosition(rb.position + dir * Movespeed * Time.deltaTime);
+            _isRunning = false;
+            _animator.SetBool("Run", false);
         }
-
-        _yVelocity += _gravity * Time.deltaTime;
-        dir.y = _yVelocity;
-        _characterController.Move(dir * _currentSpeed * Time.deltaTime);
-        _animator.SetFloat("Move", unNormalizedDir.magnitude);
     }
-
-    private void OnAnimatorMove()
+    void Jump()
     {
-        if (_owner.PhotonView.IsMine)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)	// IsGrounded가 true일 때만 점프할 수 있도록
         {
-            _characterController.Move(_animator.deltaPosition);
-            transform.rotation = _animator.rootRotation;
+            Vector3 jumpVelocity = Vector3.up * Mathf.Sqrt(JumpPower * -2f * Physics.gravity.y);
+            rb.AddForce(jumpVelocity, ForceMode.VelocityChange);
+            _animator.SetTrigger("Jump");
+            Debug.Log($"{isGrounded}");
         }
     }
-
-    public void Jump()
+    void GroundCheck()
     {
-        _isJumping = true;
-        _yVelocity = JumpPower;
+        RaycastHit hit;
+
+        // 플레이어의 위치에서, 아래방향으로, groundDistance 만큼 ray를 쏴서, ground 레이어가 있는지 검사
+        if (Physics.Raycast(LayerPoint.position, Vector3.down, out hit, groundDistance, ground))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
     }
 }

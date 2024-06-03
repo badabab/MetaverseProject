daa510dcff45c8124243016078e872d6875fe5b5
dbstjr7412369@gmail.com <dbstjr7412369@gmail.com>
2024-06-03 13:50:27 +0900

    [이윤석] 타워클라임 호박 완성

--- Assets/02.Scripts/Stage/TowerClimb/PumpkinMovement.cs
+++ Assets/02.Scripts/Stage/TowerClimb/PumpkinMovement.cs
@@ -4,52 +4,51 @@ using UnityEngine;
 
 public class PumpkinMovement : MonoBehaviour
 {
-    public GameObject pumpkin; // 플레이어 태그를 갖고 있는 오브젝트
-    public float targetYSelected = 500f; // 선택된 오브젝트가 이동할 Y 좌표
-    public float smoothSpeed = 0.1f; // 이동할 때 사용할 부드러운 이동 속도
-    public float timer = 5f; // 이동 후 대기할 시간
+    public Transform pumpkin; // 플레이어 태그를 갖고 있는 오브젝트
+    public float targetYSelected; // 선택된 오브젝트가 이동할 Y 좌표
+    public float moveSpeed; // 이동 속도
 
-    private Vector3 velocity = Vector3.zero;
     private bool isMoving = false;
-
-    void Start()
-    {
-
-    }
-
-    IEnumerator MoveToY(GameObject obj, Vector3 targetPosition, float smoothSpeed)
-    {
-        while (Vector3.Distance(obj.transform.position, targetPosition) > 0.01f)
-        {
-            obj.transform.position = Vector3.SmoothDamp(obj.transform.position, targetPosition, ref velocity, smoothSpeed * Time.deltaTime);
-            yield return null;
-        }
-    }
-
-    IEnumerator DisableAndEnablePumpkin()
-    {
-        pumpkin.SetActive(false);
-        yield return new WaitForSeconds(timer);
-        pumpkin.SetActive(true);
-    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !isMoving)
         {
             Debug.Log("충돌");
 
-            // 0 또는 1 중 랜덤하게 선택
-            int randomValue = Random.Range(0, 2);
-
-            if (randomValue == 0)
+            // 2분의 1 확률로 선택
+            if (Random.value < 0.5f)
             {
-                StartCoroutine(DisableAndEnablePumpkin());
+                isMoving = true;
+                StartCoroutine(MovePumpkin());
             }
             else
             {
-                StartCoroutine(MoveToY(pumpkin, new Vector3(pumpkin.transform.position.x, targetYSelected, pumpkin.transform.position.z), smoothSpeed));
+                StartCoroutine(DisableAndEnableObject());
             }
         }
+    }
+
+    IEnumerator MovePumpkin()
+    {
+        float elapsedTime = 0f;
+        Vector3 startPosition = pumpkin.position;
+        Vector3 targetPosition = new Vector3(pumpkin.position.x, targetYSelected, pumpkin.position.z);
+
+        while (elapsedTime < 1f)
+        {
+            elapsedTime += Time.deltaTime * moveSpeed;
+            pumpkin.position = Vector3.Lerp(startPosition, targetPosition, elapsedTime);
+            yield return null;
+        }
+
+        isMoving = false;
+    }
+
+    IEnumerator DisableAndEnableObject()
+    {
+        gameObject.SetActive(false);
+        yield return new WaitForSeconds(5f);
+        gameObject.SetActive(true);
     }
 }

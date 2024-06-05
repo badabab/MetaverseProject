a404ecc8d3b0ccf7c2024c502c0dcd63ce03a367
dbstjr7412369@gmail.com <dbstjr7412369@gmail.com>
2024-06-05 18:01:56 +0900

    [이윤석] 타워 클라임 투명도 조절

--- Assets/02.Scripts/Stage/TowerClimb/PumpkinMovement.cs
+++ Assets/02.Scripts/Stage/TowerClimb/PumpkinMovement.cs
@@ -7,7 +7,7 @@ public class PumpkinMovement : MonoBehaviour
     public Transform pumpkin; // 플레이어 태그를 갖고 있는 오브젝트
     public float targetYSelected; // 선택된 오브젝트가 이동할 Y 좌표
     public float moveSpeed; // 이동 속도
-    private float Timer = 2f;
+    private float Timer = 5f;
     private bool isMoving = false;
 
     private void OnTriggerEnter(Collider other)

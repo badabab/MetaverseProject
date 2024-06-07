f325fb5fbcb349e66d9abcbb25b5d31994c9fd08
Ohsori001106 <surnameMin@Gmail.com>
2024-06-07 13:07:51 +0900

    [이성민] 애니메이션 조건 수정

--- Assets/02.Scripts/Player/PlayerMoveAbility.cs
+++ Assets/02.Scripts/Player/PlayerMoveAbility.cs
@@ -63,6 +63,10 @@ public class PlayerMoveAbility : PlayerAbility
 
     private void FixedUpdate()
     {
+        if ( !_owner.PhotonView.IsMine)
+        {
+            return;
+        }
         InputAndDir();
     }
 

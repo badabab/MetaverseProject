a976127df3e03d50b62b7b6c07b27b9e17566ea9
공지수 <giroo27@gmail.com>
2024-06-07 11:39:28 +0900

    [공지수] 터널에 다가가면 빠르게 움직이게 수정 중

--- Assets/02.Scripts/Stage/FallGuys/TunnelObject.cs
+++ Assets/02.Scripts/Stage/FallGuys/TunnelObject.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class TunnelObject : MonoBehaviour
 {
-    public Transform OutTrans;
+    public Transform[] points; // 각 터널 객체에 대한 포인트 배열
+    public float transitionDuration = 0.1f; // 각 구간 이동 시간
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            other.transform.position = OutTrans.position;
+            StartCoroutine(TeleportPlayer(other.transform));
+        }
+    }
+
+    private IEnumerator TeleportPlayer(Transform playerTransform)
+    {
+        for (int i = 0; i < points.Length - 1; i++)
+        {
+            Vector3 startPosition = points[i].position;
+            Vector3 endPosition = points[i + 1].position;
+            float elapsedTime = 0f;
+
+            while (elapsedTime < transitionDuration)
+            {
+                playerTransform.position = Vector3.Lerp(startPosition, endPosition, elapsedTime / transitionDuration);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            playerTransform.position = endPosition;
         }
     }
 }

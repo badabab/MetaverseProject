cf468c8c5e3bf232c30a0843973c5b9822cc5bc9
공지수 <giroo27@gmail.com>
2024-06-07 11:45:44 +0900

    [공지수] 터널 지나갈때 포톤네트워크로 이동하기

--- Assets/02.Scripts/Stage/FallGuys/TunnelObject.cs
+++ Assets/02.Scripts/Stage/FallGuys/TunnelObject.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class TunnelObject : MonoBehaviour
+public class TunnelObject : MonoBehaviourPun
 {
     public Transform[] points; // 각 터널 객체에 대한 포인트 배열
     public float transitionDuration = 0.1f; // 각 구간 이동 시간
@@ -11,11 +12,15 @@ public class TunnelObject : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            StartCoroutine(TeleportPlayer(other.transform));
+            PhotonView photonView = other.GetComponent<PhotonView>();
+            if (photonView != null && photonView.IsMine)
+            {
+                StartCoroutine(TeleportPlayer(other.transform, photonView));
+            }
         }
     }
 
-    private IEnumerator TeleportPlayer(Transform playerTransform)
+    private IEnumerator TeleportPlayer(Transform playerTransform, PhotonView photonView)
     {
         for (int i = 0; i < points.Length - 1; i++)
         {
@@ -31,6 +36,19 @@ public class TunnelObject : MonoBehaviour
             }
 
             playerTransform.position = endPosition;
+        }
+
+        // 플레이어의 최종 위치를 다른 클라이언트에 동기화
+        photonView.RPC("UpdatePlayerPosition", RpcTarget.All, playerTransform.position);
+    }
+
+    [PunRPC]
+    void UpdatePlayerPosition(Vector3 newPosition)
+    {
+        Transform playerTransform = PhotonView.Find(photonView.ViewID).transform;
+        if (playerTransform != null && !photonView.IsMine)
+        {
+            playerTransform.position = newPosition;
         }
     }
 }

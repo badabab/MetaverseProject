dedebc752a0096fe2b09bceab5e1e40d9be4eeac
Ohsori001106 <surnameMin@Gmail.com>
2024-06-10 14:54:40 +0900

    [이성민] 카메라

--- Assets/02.Scripts/Camera/TPSCamera.cs
+++ Assets/02.Scripts/Camera/TPSCamera.cs
@@ -40,10 +40,7 @@ public class TPSCamera : MonoBehaviourPunCallbacks
     void Update()
     {
 
-        if (!_owner.PhotonView.IsMine)
-        {
-            return;
-        }
+
         if (target == null) return; // 타겟이 없으면 리턴
 
         rotationX += Input.GetAxis("Mouse X") * sensitivity;

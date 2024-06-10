743f296bd214a6578a6b2e480a6c0528f85c3660
badabab <ky040723@gmail.com>
2024-06-10 13:19:08 +0900

    [김예은] 플레이어 효과 만드는 중

--- Assets/02.Scripts/UI/UI_Lobby.cs
+++ Assets/02.Scripts/UI/UI_Lobby.cs
@@ -16,7 +16,8 @@ public class UI_Lobby : MonoBehaviour
     public TMP_InputField TMP_InputFieldId;
     public TMP_InputField TMP_InputFieldPw;
     public Toggle RememberToggle;
-    public TMP_InputField TMP_InputField;
+    public TMPro.TMP_InputField TMP_InputField;
+    // public InputField NicknameInputFieldUI;
     public Button NextButtonUI;
     public Button FemaleButtonUI;
     public Button MaleButtonUI;
@@ -26,53 +27,24 @@ public class UI_Lobby : MonoBehaviour
     public string RoomID = "testRoom";
     public static PlayerType SelectedType = PlayerType.Male;
 
+    private GameObject _character;
+    public GameObject[] SelectedCharacter;
+
+
     private void Start()
     {
-        LoadLoginInfo();
+        string loggedInUser = PlayerPrefs.GetString("LoggedInId", null);
+        string loggedInPassword = PlayerPrefs.GetString("LoggedInPassword", null);
+        if (PlayerPrefs.HasKey(loggedInUser))
+        {
+            TMP_InputFieldId.text = $"{loggedInUser}";
+        }
+        else if (PlayerPrefs.HasKey(loggedInPassword))
+        {
+            TMP_InputFieldPw.text = $"{loggedInPassword}";
+        }
+
         AutoLogin();
-    }
-
-    private void LoadLoginInfo()
-    {
-        string loggedInUser = PlayerPrefs.GetString("LoggedInId", string.Empty);
-        string loggedInPassword = PlayerPrefs.GetString("LoggedInPassword", string.Empty);
-        TMP_InputFieldId.text = loggedInUser;
-        TMP_InputFieldPw.text = loggedInPassword;
-    }
-
-    private void AutoLogin()
-    {
-        string loggedInUser = PlayerPrefs.GetString("LoggedInId", string.Empty);
-        string loggedInPassword = PlayerPrefs.GetString("LoggedInPassword", string.Empty);
-
-        if (!string.IsNullOrEmpty(loggedInUser) && !string.IsNullOrEmpty(loggedInPassword))
-        {
-            var user = PersonalManager.Instance.Login(loggedInUser, loggedInPassword);
-            if (user != null)
-            {
-                PhotonNetwork.NickName = loggedInUser;
-                PlayerSelection.Instance.SelectedCharacterIndex = user.CharacterIndex;
-
-                if (user.CharacterIndex != 0)
-                {
-                    PlayerSelection.Instance.ReloadCharacter();
-                    OnClickStartButton();
-                }
-                else
-                {
-                    Metaverse1.SetActive(false);
-                    Metaverse2.SetActive(true);
-                }
-            }
-            else
-            {
-                Metaverse1.SetActive(true);
-            }
-        }
-        else
-        {
-            Metaverse1.SetActive(true);
-        }
     }
 
     public void OnClickNextButton()
@@ -86,43 +58,109 @@ public class UI_Lobby : MonoBehaviour
             return;
         }
 
-        if (RememberToggle.isOn)
+
+        RememberID rememberID = RememberToggle.isOn ? RememberID.Remember : RememberID.Nope;
+
+
+        if (rememberID == RememberID.Remember)
         {
-            RememberUserInfo(nickname, password);
+            if (!PersonalManager.Instance.CheckUser(nickname, password))
+            {
+                int index = PlayerSelection.Instance.SelectedCharacterIndex;
+                PersonalManager.Instance.JoinList(nickname, password, index);
+                Debug.Log("New user registered.");
+            }
+
+            var user = PersonalManager.Instance.Login(nickname, password);
+            if (user != null)
+            {
+                PlayerPrefs.SetString("LoggedInId", nickname);
+                PlayerPrefs.SetString("LoggedInPassword", password);
+                Debug.Log("Login successful, user remembered.");
+                PhotonNetwork.NickName = nickname;
+            }
         }
 
-        var user = PersonalManager.Instance.Login(nickname, password);
-        if (user != null)
+        else if (rememberID == RememberID.Nope)
         {
-            PhotonNetwork.NickName = nickname;
-            PlayerSelection.Instance.SelectedCharacterIndex = user.CharacterIndex;
-            OnClickStartButton();
+            var user = PersonalManager.Instance.Login(nickname, password);
+            if (user != null)
+            {
+                Debug.Log("Login successful.");
+                PhotonNetwork.NickName = nickname;
+            }
+            else
+            {
+                Debug.Log("Login failed.");
+            }
+        }
+
+        PhotonNetwork.NickName = nickname;
+
+        // Metaverse1 비활성화, Metaverse2 활성화
+        Metaverse1.SetActive(false);
+        Metaverse2.SetActive(true);
+    }
+    private void AutoLogin()
+    {
+        string loggedInUser = PlayerPrefs.GetString("LoggedInId", null);
+        string loggedInPassword = PlayerPrefs.GetString("LoggedInPassword", null);
+
+        if (!string.IsNullOrEmpty(loggedInUser) && !string.IsNullOrEmpty(loggedInPassword))
+        {
+            TMP_InputFieldId.text = loggedInUser;
+            TMP_InputFieldPw.text = loggedInPassword;
+            var user = PersonalManager.Instance.Login(loggedInUser, loggedInPassword);
+            if (user != null)
+            {
+                PhotonNetwork.NickName = loggedInUser;
+
+                if (user.CharacterIndex == 0)
+                {
+                    Metaverse1.SetActive(false);
+                    Metaverse2.SetActive(true);
+                }
+                else
+                {
+                    OnClickStartButton();
+                   // GoToLoadingScene();
+                }
+            }
+            else
+            {
+                // 로그인 실패 시 ID/PW 입력 창 표시
+                Metaverse1.SetActive(true);
+            }
         }
         else
         {
-            Debug.Log("Login failed.");
+            // 로컬에 저장된 ID/PW가 없는 경우 ID/PW 입력 창 표시
+            Metaverse1.SetActive(true);
         }
     }
 
-    private void RememberUserInfo(string nickname, string password)
+/*    public void GoToLoadingScene()
     {
-        if (!PersonalManager.Instance.CheckUser(nickname, password))
+        RoomOptions roomOptions = new RoomOptions
         {
-            int index = PlayerSelection.Instance.SelectedCharacterIndex;
-            PersonalManager.Instance.JoinList(nickname, password, index);
-            Debug.Log("New user registered.");
-        }
+            MaxPlayers = 20,
+            IsVisible = true,
+            IsOpen = true,
+            EmptyRoomTtl = 1000 * 20,
+            CustomRoomProperties = new ExitGames.Client.Photon.Hashtable { { "MasterNickname", PhotonNetwork.NickName } },
+            CustomRoomPropertiesForLobby = new string[] { "MasterNickname" }
+        };
 
-        PlayerPrefs.SetString("LoggedInId", nickname);
-        PlayerPrefs.SetString("LoggedInPassword", password);
-        Debug.Log("Login successful, user remembered.");
-    }
+        PhotonNetwork.JoinOrCreateRoom(RoomID, roomOptions, TypedLobby.Default);
+        PlayerSelection.Instance.ReloadCharacter();
+        SceneManager.LoadScene("LoadingScene");
+    }*/
 
     public void OnClickStartButton()
     {
+        // Metaverse1 비활성화, Metaverse2 활성화
         Metaverse1.SetActive(false);
         Metaverse2.SetActive(true);
-
         if (PhotonNetwork.IsConnectedAndReady)
         {
             RoomOptions roomOptions = new RoomOptions
@@ -131,17 +169,19 @@ public class UI_Lobby : MonoBehaviour
                 IsVisible = true,
                 IsOpen = true,
                 EmptyRoomTtl = 1000 * 20,
+                CustomRoomProperties = new ExitGames.Client.Photon.Hashtable { { "MasterNickname", PhotonNetwork.NickName } },
+                CustomRoomPropertiesForLobby = new string[] { "MasterNickname" },
             };
 
             PhotonNetwork.JoinOrCreateRoom(RoomID, roomOptions, TypedLobby.Default);
             Debug.Log($"{RoomID}");
+            PlayerSelection.Instance.ReloadCharacter();
         }
         else
         {
             Debug.LogWarning("PhotonNetwork is not ready.");
         }
     }
-
     public void OnClickMaleButton() => OnClickPlayerTypeButton(PlayerType.Male);
     public void OnClickFemaleButton() => OnClickPlayerTypeButton(PlayerType.Female);
 
@@ -153,9 +193,10 @@ public class UI_Lobby : MonoBehaviour
 
     public void OnNicknameValueChanged(string newValue)
     {
-        if (!string.IsNullOrEmpty(newValue))
+        if (string.IsNullOrEmpty(newValue))
         {
-            PhotonNetwork.NickName = newValue;
+            return;
         }
+        PhotonNetwork.NickName = newValue;
     }
 }

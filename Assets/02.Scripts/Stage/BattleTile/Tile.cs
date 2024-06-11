897b780f6a599e392c3906b78edf80d7df2aada8
badabab <ky040723@gmail.com>
2024-06-11 12:47:32 +0900

    [김예은] 배틀타일 UI 복구 완료

--- Assets/02.Scripts/Stage/BattleTile/Tile.cs
+++ Assets/02.Scripts/Stage/BattleTile/Tile.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using Photon.Pun;
-using System.Collections;
 
 public class Tile : MonoBehaviourPunCallbacks
 {
@@ -46,22 +45,6 @@ public class Tile : MonoBehaviourPunCallbacks
     [PunRPC]
     private void ChangeMaterial(int playerNum)
     {
-        StartCoroutine(ChangeMaterialCoroutine(Materials[playerNum]));
-    }
-
-    private IEnumerator ChangeMaterialCoroutine(Material targetMaterial)
-    {
-        Material startMaterial = _renderer.material;
-        float duration = 1.0f; // 전환 시간 (초)
-        float elapsedTime = 0f;
-
-        while (elapsedTime < duration)
-        {
-            elapsedTime += Time.deltaTime;
-            _renderer.material.Lerp(startMaterial, targetMaterial, elapsedTime / duration);
-            yield return null;
-        }
-
-        _renderer.material = targetMaterial; // 최종 Material을 설정
+        _renderer.material = Materials[playerNum];
     }
 }

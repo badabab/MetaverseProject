90623025ea815538b7e50670276be5ddff4cd079
공지수 <giroo27@gmail.com>
2024-06-12 18:11:45 +0900

    [공지수] 다음씬에서 불러오다가 집에 감

--- Assets/02.Scripts/Stage/TowerClimb/GameOverBall.cs
+++ Assets/02.Scripts/Stage/TowerClimb/GameOverBall.cs
@@ -3,23 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
-public class GameOverBall : MonoBehaviour
+public class GameOverBall : MonoBehaviourPunCallbacks
 {
     public GameObject GameOverUI;
     public GameObject GameOver_testUI;
 
     private bool playerCollided = false;
 
+    private string _firstPlayer;
+
     private void OnTriggerEnter(Collider other)
     {
+        if(!PhotonNetwork.IsMasterClient)
+        {
+            return;
+        }
+
         if (other.CompareTag("Player") && !playerCollided)
         {
             playerCollided = true;
             Time.timeScale = 0f;
             StartCoroutine(GameOverSequence(other.gameObject));
+            _firstPlayer = other.name;
+
+            if (PhotonNetwork.IsMasterClient ) 
+            {
+                Hashtable firstPlayerName = new Hashtable { { "FirstPlayerName", _firstPlayer } };
+                PhotonNetwork.CurrentRoom.SetCustomProperties(firstPlayerName);
+            }
         }
     }
+
+    
+
+
+
     private IEnumerator GameOverSequence(GameObject Player)
     {
         GameOverUI.SetActive(true);
@@ -33,7 +53,7 @@ public class GameOverBall : MonoBehaviour
             animator.SetBool("Win", true);
             PersonalManager.Instance.CoinUpdate(photonView.Owner.NickName, 100);
             PhotonNetwork.LoadLevel("TowerClimbWinScene");
-            PhotonNetwork.LeaveRoom();
+            //PhotonNetwork.LeaveRoom();
         }
         Time.timeScale = 1f;
     }
--- Assets/02.Scripts/Stage/TowerClimb/WinningPlayerScene.cs
+++ Assets/02.Scripts/Stage/TowerClimb/WinningPlayerScene.cs
@@ -6,10 +6,19 @@ using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class WinningPlayerScene : MonoBehaviour
 {
+    private string _firstName;
+
+    public Vector3 PlayerSpawn;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("FirstPlayerName"))
+        {
+            _firstName = (string)PhotonNetwork.CurrentRoom.CustomProperties["FirstPlayerName"];
+            int characterIndex = PersonalManager.Instance.CheckCharacterIndex();
+
+            PhotonNetwork.Instantiate(_firstName, PlayerSpawn, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame

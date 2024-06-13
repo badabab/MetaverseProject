fde8f82b31df45500a7acd90583e26e8c46639fa
공지수 <giroo27@gmail.com>
2024-06-13 15:49:20 +0900

    [공지수] 위닝씬에서 플레이어 소환하고 카메라 수정하는거 함

--- Assets/02.Scripts/Stage/TowerClimb/WinningPlayerScene.cs
+++ Assets/02.Scripts/Stage/TowerClimb/WinningPlayerScene.cs
@@ -1,52 +1,86 @@
 using Photon.Pun;
-using System.Linq;
+using System.Collections;
 using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class WinningPlayerScene : MonoBehaviour
 {
-    private string _firstName;
+    private string _firstPlayerName;
 
-    public Vector3 PlayerSpawn;
-
+    public Transform PlayerSpawn;
     public TextMeshProUGUI WinningName;
+    private GameObject winningPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
         if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("FirstPlayerName"))
         {
-            _firstName = (string)PhotonNetwork.CurrentRoom.CustomProperties["FirstPlayerName"];
-            int characterIndex = PersonalManager.Instance.CheckCharacterIndex();
-            string nickname = PersonalManager.Instance.UserNameMach();
+            _firstPlayerName = (string)PhotonNetwork.CurrentRoom.CustomProperties["FirstPlayerName"];
 
-            if (_firstName == nickname)
+            // 첫 번째 플레이어만 생성
+            if (PhotonNetwork.NickName == _firstPlayerName)
             {
+                int characterIndex = PersonalManager.Instance.CheckCharacterIndex();
                 string firstCharacter = $"Player {characterIndex}";
-                PhotonNetwork.Instantiate(firstCharacter, PlayerSpawn, Quaternion.identity);
-                WinningName.text = _firstName;
+                winningPlayer = PhotonNetwork.Instantiate(firstCharacter, PlayerSpawn.position, Quaternion.identity);
+                WinningName.text = _firstPlayerName;
+
+                Animator animator = winningPlayer.GetComponent<Animator>();
+                animator.SetBool("Win", true);
+
+                // 카메라 설정
+                SetupCameraForPlayer(winningPlayer);
+            }
+        }
+        else
+        {
+            // 첫 번째 플레이어가 아닌 경우 카메라 설정
+            FindAndSetupCameraForFirstPlayer();
+        }
+
+        StartCoroutine(AllPlayerOut_Coroutine());
+    }
+
+    private void SetupCameraForPlayer(GameObject player)
+    {
+        Transform cameraRoot = player.transform.Find("CameraRoot");
+        if (cameraRoot != null)
+        {
+            TPSCamera tpsCamera = FindObjectOfType<TPSCamera>();
+            if (tpsCamera != null)
+            {
+                tpsCamera.target = cameraRoot;
             }
             else
             {
-                return;
+                Debug.LogError("TPSCamera not found in scene.");
+            }
+        }
+        else
+        {
+            Debug.LogError("CameraRoot not found on player: " + player.name);
+        }
+    }
+
+    private void FindAndSetupCameraForFirstPlayer()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject player in players)
+        {
+            PhotonView photonView = player.GetComponent<PhotonView>();
+            if (photonView != null && photonView.Owner.NickName == _firstPlayerName)
+            {
+                SetupCameraForPlayer(player);
+                break;
             }
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator AllPlayerOut_Coroutine()
     {
-        
-    }
-
-    public void AllPlayerCameraOff()
-    {
-        Photon.Realtime.Player[] players = PhotonNetwork.PlayerList.ToArray();
-        Debug.Log("Player count: " + players.Length);
-        foreach (Photon.Realtime.Player player in players)
-        {
-
-        }
+        yield return new WaitForSeconds(5);
+        PhotonNetwork.LoadLevel("VillageScene");
+        PhotonNetwork.LeaveRoom();
     }
 }

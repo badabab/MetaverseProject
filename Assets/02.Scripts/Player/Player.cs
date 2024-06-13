49ea77db020cc982bbb844c1ec9a4a6e0f69cd91
badabab <ky040723@gmail.com>
2024-06-13 17:39:37 +0900

    [김예은] 마을에서 8,9,0 누르면 포탈 앞으로 이동하는 임시코드 넣어놓음

--- Assets/02.Scripts/Player/Player.cs
+++ Assets/02.Scripts/Player/Player.cs
@@ -1,6 +1,8 @@
 using Photon.Pun;
 using UnityEngine;
 using Photon.Realtime;
+using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(PlayerMoveAbility))]
 [RequireComponent(typeof(PlayerRotateAbility))]
@@ -22,5 +24,29 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
     }
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
+    }
+
+
+
+
+
+    // 임시 이동 코드
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "VillageScene")
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha8)) // 타일 MiniGame1
+            {              
+                this.transform.position = new Vector3(-84, 13, 110);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha9)) // 짭가이즈 MiniGame2
+            {                                
+                this.transform.position = new Vector3(75, 10, 20);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha0)) // 타워 MiniGame3
+            {                               
+                this.transform.position = new Vector3(-80, 10, -105);
+            }
+        }
     }
 }

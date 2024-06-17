f354a413e6512780e8a9c17908a19e79a62a58c1
공지수 <giroo27@gmail.com>
2024-06-17 15:09:35 +0900

    [공지수] 계속하기/ 정지하기 스크립트 수정

--- Assets/02.Scripts/Player/PlayerOptionAbility.cs
+++ Assets/02.Scripts/Player/PlayerOptionAbility.cs
@@ -6,12 +6,35 @@ public class PlayerOptionAbility : PlayerAbility
     public void Pause()
     {
         if (!_owner.photonView.IsMine) return;
-        Time.timeScale = 0f;
+        photonView.RPC("RPC_Pause", RpcTarget.AllBuffered, _owner.photonView.IsMine);
     }
 
     public void Continue()
     {
         if (!_owner.photonView.IsMine) return;
-        Time.timeScale = 1f;
+        photonView.RPC("RPC_Continue", RpcTarget.AllBuffered, _owner.photonView.IsMine);
+    }
+
+    [PunRPC]
+    private void RPC_Pause(PhotonView photonView)
+    {
+        if (photonView != null && photonView.IsMine)
+        {
+            if (photonView.gameObject.TryGetComponent<PlayerMoveAbility>(out var moveAbility))
+            {
+                moveAbility.enabled = false;
+            }
+        }
+    }
+    [PunRPC]
+    private void RPC_Continue(PhotonView photonView)
+    {
+        if (photonView != null && photonView.IsMine)
+        {
+            if (photonView.gameObject.TryGetComponent<PlayerMoveAbility>(out var moveAbility))
+            {
+                moveAbility.enabled = true;
+            }
+        }
     }
 }

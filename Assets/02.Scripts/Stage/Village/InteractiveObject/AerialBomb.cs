5117bb9352414c4124b75787c3950b0e3744e48b
dbstjr7412369@gmail.com <dbstjr7412369@gmail.com>
2024-06-17 13:45:13 +0900

    [이윤석]인터렉티브 오브젝트 제작 중

--- Assets/02.Scripts/Stage/Village/InteractiveObject/AerialBomb.cs
+++ Assets/02.Scripts/Stage/Village/InteractiveObject/AerialBomb.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class AerialBomb : MonoBehaviourPunCallbacks, IPunObservable
 {
-    public GameObject explosionEffectPrefab; // 폭발 효과 프리팹
-    public float BombFall = 8.5f; // 폭발까지의 지연 시간
-    public float BombTime = 6f; // 폭발 효과 지속 시간
+    public GameObject explosionEffectPrefab; 
+    public float BombFall = 8.5f; 
+    public float BombTime = 6f; 
 
-    private bool hasExploded = false; // 폭발 여부
+    private bool hasExploded = false; 
 
     void Start()
     {
@@ -29,15 +29,12 @@ public class AerialBomb : MonoBehaviourPunCallbacks, IPunObservable
     [PunRPC]
     void Explode()
     {
-        // 폭발 효과를 생성하고 모든 클라이언트에게 동기화합니다.
         GameObject explosion = Instantiate(explosionEffectPrefab, transform.position, transform.rotation);
 
-        // BombTime 시간 후에 폭발 효과를 파괴합니다.
         Destroy(explosion, BombTime);
 
         hasExploded = true;
 
-        // 폭탄이 소유한 클라이언트만 폭탄 게임 오브젝트를 파괴합니다.
         if (photonView.IsMine)
         {
             PhotonNetwork.Destroy(gameObject);
@@ -46,6 +43,6 @@ public class AerialBomb : MonoBehaviourPunCallbacks, IPunObservable
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        // 이 예제에서는 네트워크 동기화가 필요하지 않으므로 구현하지 않습니다.
+
     }
 }

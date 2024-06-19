4e3bef0236d4e1dcb24bd1bb7380a8bb61084dfe
공지수 <giroo27@gmail.com>
2024-06-19 15:26:11 +0900

    [공지수] 포톤네트워크 입장 다시 수정

--- Assets/02.Scripts/Stage/Village/VillageTutorial.cs
+++ Assets/02.Scripts/Stage/Village/VillageTutorial.cs
@@ -44,46 +44,10 @@ public class VillageTutorial : MonoBehaviourPunCallbacks
 
     private void LoadVillageScene()
     {
-        PhotonNetwork.LoadLevel("VillageScene");
-        /*if (!isLeavingRoom)
-        {
-            isLeavingRoom = true;
-            if (PhotonNetwork.InRoom)
-            {
-                PhotonNetwork.LeaveRoom(); // 방을 떠나고 OnLeftRoom 콜백에서 새로운 방에 참가
-            }
-            else
-            {
-                JoinOrCreateRoom(); // 방에 없으면 바로 새로운 방에 참가
-            }
-        }*/
+        Debug.Log($"방 입장 성공! : ({PhotonNetwork.CurrentRoom.Name})");
+        PhotonNetwork.IsMessageQueueRunning = false; // 메시지 큐 일시 정지
+        PhotonNetwork.LoadLevel("VillageScene"); // PhotonNetwork를 통해 씬 로드
+        PhotonNetwork.IsMessageQueueRunning = true; // 메시지 큐 재개
     }
 
-/*    public override void OnLeftRoom()
-    {
-        Debug.Log("Left room. Now joining or creating room: " + RoomID);
-        JoinOrCreateRoom();
-    }
-
-    private void JoinOrCreateRoom()
-    {
-        if (PhotonNetwork.IsConnectedAndReady)
-        {
-            RoomOptions roomOptions = new RoomOptions
-            {
-                MaxPlayers = 20,
-                IsVisible = true,
-                IsOpen = true,
-                EmptyRoomTtl = 1000 * 20,
-            };
-            PhotonNetwork.JoinOrCreateRoom(RoomID, roomOptions, TypedLobby.Default);
-            Debug.Log($"{RoomID}");
-            isLeavingRoom = false; // 방 참가 시도를 했으므로 플래그 리셋
-        }
-        else
-        {
-            Debug.LogWarning("PhotonNetwork is not ready.");
-            isLeavingRoom = false; // 실패했으므로 플래그 리셋
-        }
-    }*/
 }

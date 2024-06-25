b35b2175b88f1f1f3fc8b9e1e0c959b64a69014d
badabab <ky040723@gmail.com>
2024-06-25 16:27:43 +0900

    [김예은] 로비 로고 코드

--- Assets/02.Scripts/UI/UI_Lobby.cs
+++ Assets/02.Scripts/UI/UI_Lobby.cs
@@ -1,5 +1,5 @@
 using Photon.Pun;
-using Photon.Realtime;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -12,8 +12,8 @@ public enum PlayerType
 }
 public class UI_Lobby : MonoBehaviour
 {
-    public GameObject WhiteBack;
-    public GameObject Logo;
+    public Image WhiteBack;
+    public Image Logo;
     public TMP_InputField TMP_InputFieldId;
     public TMP_InputField TMP_InputFieldPw;
     public Toggle RememberToggle;
@@ -23,7 +23,6 @@ public class UI_Lobby : MonoBehaviour
     public Button MaleButtonUI;
     public GameObject Metaverse1;
     public GameObject Metaverse2;
-   
 
     //private string RoomID = "Village";
     public static PlayerType SelectedType = PlayerType.Male;
@@ -32,54 +31,42 @@ public class UI_Lobby : MonoBehaviour
 
     private void Start()
     {
-        LoadLoginInfo();
-        AutoLogin();
+        Metaverse1.SetActive(false);
+        StartCoroutine(FadeOutWhiteBackAndExecute());
     }
-    
+
+    private IEnumerator FadeOutWhiteBackAndExecute()
+    {
+        float duration = 5.0f;
+        float elapsedTime = 0.0f;
+        float startAlpha = 200 / 255.0f; // 200을 0~1 범위로 변환      
+
+        Color color = WhiteBack.color;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float newAlpha = Mathf.Lerp(startAlpha, 0, elapsedTime / duration);
+            WhiteBack.color = new Color(color.r, color.g, color.b, newAlpha);
+            yield return null;
+        }
+
+        WhiteBack.color = new Color(color.r, color.g, color.b, 0);
+
+        yield return new WaitForSeconds(1);
+
+        Logo.gameObject.SetActive(false);
+        WhiteBack.gameObject.SetActive(false);
+        Metaverse1.SetActive(true);
+        LoadLoginInfo();
+        //AutoLogin();
+    }
+
     private void LoadLoginInfo()
     {
         string loggedInUser = PlayerPrefs.GetString("LoggedInId", string.Empty);
         string loggedInPassword = PlayerPrefs.GetString("LoggedInPassword", string.Empty);
         TMP_InputFieldId.text = loggedInUser;
         TMP_InputFieldPw.text = loggedInPassword;
-    }
-
-    private void AutoLogin()
-    {
-
-        string loggedInUser = PlayerPrefs.GetString("LoggedInId", string.Empty);
-        string loggedInPassword = PlayerPrefs.GetString("LoggedInPassword", string.Empty);
-
-        if (!string.IsNullOrEmpty(loggedInUser) && !string.IsNullOrEmpty(loggedInPassword))
-        {
-            var user = PersonalManager.Instance.Login(loggedInUser, loggedInPassword);
-            if (user != null)
-            {
-                PhotonNetwork.NickName = loggedInUser;
-                PlayerSelection.Instance.SelectedCharacterIndex = user.CharacterIndex;
-
-                if (user.CharacterIndex != 0)
-                {
-                    PlayerSelection.Instance.ReloadCharacter();
-                    SelectCharacterBrowser();
-                    FemaleButtonUI.gameObject.SetActive(false);
-                    MaleButtonUI.gameObject.SetActive(false);
-                }
-                else
-                {
-                    SelectCharacterBrowser();
-                }
-            }
-            else
-            {
-                Metaverse1.SetActive(true);
-            }
-        }
-
-        else
-        {
-            Metaverse1.SetActive(true);
-        }
     }
 
     public void OnClickNextButton()
@@ -142,8 +129,6 @@ public class UI_Lobby : MonoBehaviour
         {
             //SceneManager.LoadScene("LoadingScene");
             SceneManager.LoadScene("VillageSceneTutorials");
-            
-           // PhotonNetwork.LoadLevel("LoadingScene");
         }
         else
         {
@@ -158,5 +143,42 @@ public class UI_Lobby : MonoBehaviour
     {
         SelectedType = Ptype;
         PlayerSelection.Instance.CharacterSelection(Ptype);
+    }
+
+    private void AutoLogin()
+    {
+
+        string loggedInUser = PlayerPrefs.GetString("LoggedInId", string.Empty);
+        string loggedInPassword = PlayerPrefs.GetString("LoggedInPassword", string.Empty);
+
+        if (!string.IsNullOrEmpty(loggedInUser) && !string.IsNullOrEmpty(loggedInPassword))
+        {
+            var user = PersonalManager.Instance.Login(loggedInUser, loggedInPassword);
+            if (user != null)
+            {
+                PhotonNetwork.NickName = loggedInUser;
+                PlayerSelection.Instance.SelectedCharacterIndex = user.CharacterIndex;
+
+                if (user.CharacterIndex != 0)
+                {
+                    PlayerSelection.Instance.ReloadCharacter();
+                    SelectCharacterBrowser();
+                    FemaleButtonUI.gameObject.SetActive(false);
+                    MaleButtonUI.gameObject.SetActive(false);
+                }
+                else
+                {
+                    SelectCharacterBrowser();
+                }
+            }
+            else
+            {
+                Metaverse1.SetActive(true);
+            }
+        }
+        else
+        {
+            Metaverse1.SetActive(true);
+        }
     }
 }

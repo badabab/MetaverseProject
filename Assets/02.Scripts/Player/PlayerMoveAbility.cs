f27dd2220a0c7fc568b21cf2c379bbbc111b7273
Ohsori001106 <surnameMin@Gmail.com>
2024-07-01 12:44:01 +0900

    [이성민] 망치 해줌

--- Assets/02.Scripts/Player/PlayerMoveAbility.cs
+++ Assets/02.Scripts/Player/PlayerMoveAbility.cs
@@ -179,10 +179,12 @@ public class PlayerMoveAbility : PlayerAbility
             // 걷기 애니메이션 설정
             _animator.SetBool("Walk", true);
 
+            SoundManager.instance.StopSfx(SoundManager.Sfx.PlayerWalking);
         }
         else // 키 입력이 없는 경우
         {
             _animator.SetBool("Walk", false);
+            SoundManager.instance.StopSfx(SoundManager.Sfx.PlayerWalking);
         }
 
         direction.y = 0f;
@@ -199,6 +201,7 @@ public class PlayerMoveAbility : PlayerAbility
             _isRunning = true;
 
             _animator.SetBool("Run", true);
+            //PlayWalkVFX();
         }
         else
         {
